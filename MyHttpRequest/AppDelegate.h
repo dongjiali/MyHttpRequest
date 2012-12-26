efc529fa50ed1ccd8b6f385e238b4323e153cd5c
Dong JiaLi <Dongjiali@DongmatoiMac.local>
2012-12-26 14:35:21 +0800

    Initial Commit

---
+++ MyHttpRequest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyHttpRequest
+//
+//  Created by Dong JiaLi on 12-12-26.
+//  Copyright (c) 2012年 Dong JiaLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyHttpRequest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyHttpRequest
+//
+//  Created by Dong JiaLi on 12-12-26.
+//  Copyright (c) 2012年 Dong JiaLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
